5d76db7b6ec91590ae61c71aefe5284a37dfbbef
zade-softwareengineering <zmuhamm1@uncc.edu>
2021-03-11 20:13:15 -0500

    hehehe

--- app/src/main/java/com/example/githomework/MainActivity.java
+++ app/src/main/java/com/example/githomework/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("this is a test to see if i push/pull.");
+        System.out.println("this is a test to see if i push/pull. hehehehe");
     }
 }
